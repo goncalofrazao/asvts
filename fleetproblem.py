e44415a4139b34c449910d833218d282f1a96f2c
Gonçalo Frazão <gonfrazao@gmail.com>
2023-09-13 11:29:16 +0100

    fixed load from fleetproblem class to receive an object file instead of name of file

--- fleetproblem.py
+++ fleetproblem.py
@@ -11,28 +11,27 @@ class FleetProblem:
 
     def load(self, fh):
         self.__init__()
-        with open(fh) as fp:
-            lines = fp.readlines()
-            p = 0
-            r = 0
-            v = 0
-            for line in lines:
-                if p > 0:
-                    self.travel_time_matrix[self.P - p - 1] = [0] * (self.P - p) + [int(x) for x in line.split(' ') if x]
-                    p -= 1
-                elif r > 0:
-                    self.requests.append([int(x) for x in line.split(' ')])
-                    r -= 1
-                elif v > 0:
-                    self.vehicles.append(int(line))
-                    v -= 1
-                elif line.startswith('P'):
-                    self.P = int(line[2:])
-                    self.travel_time_matrix = [[0] * self.P] * self.P
-                    p = self.P - 1
-                elif line.startswith('R'):
-                    self.R = int(line[2:])
-                    r = self.R
-                elif line.startswith('V'):
-                    self.V = int(line[2:])
-                    v = self.V
+        lines = fh.readlines()
+        p = 0
+        r = 0
+        v = 0
+        for line in lines:
+            if p > 0:
+                self.travel_time_matrix[self.P - p - 1] = [0] * (self.P - p) + [int(x) for x in line.split(' ') if x]
+                p -= 1
+            elif r > 0:
+                self.requests.append([int(x) for x in line.split(' ')])
+                r -= 1
+            elif v > 0:
+                self.vehicles.append(int(line))
+                v -= 1
+            elif line.startswith('P'):
+                self.P = int(line[2:])
+                self.travel_time_matrix = [[0] * self.P] * self.P
+                p = self.P - 1
+            elif line.startswith('R'):
+                self.R = int(line[2:])
+                r = self.R
+            elif line.startswith('V'):
+                self.V = int(line[2:])
+                v = self.V
