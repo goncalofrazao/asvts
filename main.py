a16043e2e8aab006cc1b6f05d7888e69c905c285
Gonçalo Frazão <gonfrazao@gmail.com>
2023-10-12 18:45:19 +0100

    SUBMIT: assignment #2: submission #6

--- main.py
+++ main.py
@@ -19,30 +19,6 @@ def main():
         sol = fp.solve()
         print(fp.cost(sol))
 
-        # solution = sol.solution()
-        # path = sol.path()
-        # for i in path:
-        #     print(i.state.requests)
-        # print()
-        # print('Path')
-        # for i in path:
-        #     print(i.path_cost)
-        # print()
-        # print('Solution')
-        # for i in solution:
-        #     print(i)
-        # print()
-        # print('Cost')
-        # print(fp.cost(solution))
-        # print()
-        # print('Requests')
-        # for i in fp.requests:
-        #     print(i)
-        # print()
-        # print('Matrix')
-        # for line in fp.matrix:
-        #     print(line)
-
     if len(args) == 3:
         list = read_solution(open(args[1]))
         datfile = args[1].replace('.plan', '.dat')
