454f3d3f9a29d7360be2ae6b73f435c967908ca6
Gonçalo Frazão <gonfrazao@gmail.com>
2023-10-26 13:21:59 +0100

    new state new promise

--- solution.py
+++ solution.py
@@ -124,116 +124,63 @@ class Heap(utils.PriorityQueue):
 
 # search.PriorityQueue = Heap
 
-class State(tuple):
+class State():
+    def __init__(self, req_state: tuple, cars: dict, car_set: list):
+        self.req_state = req_state
+        self.cars = cars
+        self.car_set = car_set
+    
     def __lt__(self, other):
-        return sum([x[0] for x in self]) > sum([x[0] for x in other])
+        return sum(i[0] for i in self.req_state) < sum(i[0] for i in other.req_state)
+    
+    def __eq__(self, other):
+        return tuple(i[:2] for i in self.req_state) == tuple(i[:2] for i in other.req_state) and self.car_set == other.car_set
+    
+    def __hash__(self):
+        return hash(tuple(i[:2] for i in self.req_state) + tuple(self.car_set))
 
 class FleetProblem(search.Problem):
-    
-    def __init__(self, fh=None):
-        """Constructor method that initializes the attributes of the FleetProblem 
-        object and loads a problem instance from file object fh if it is provided.
-    
-        Usage:
-            If there is a file object, this must be opened in read mode and the file must be
-            formatted as follows without a specific order of the sections:
-                P <number of points>
-                <matrix of distances>
-                R <number of requests>
-                <requests>
-                V <number of vehicles>
-                <vehicle capacities>
-                Where:
-                    <matrix of distances> is a upper triangular matrix of floats or integers
-                    <requests> are represented by a line with the following format:
-                        <request time> <pickup point> <dropoff point> <number of passengers>
-                    <vehicle capacities> are represented by a line with a single integer
+    counter = 0
 
-        Args:
-            fh (file object): File object containing the problem instance.
-
-        """
+    def __init__(self):
         self.P = 0
         self.R = 0
         self.V = 0
-        # matrix is a square symetric matrix
-        # where matrix[i][j] is the distance between points i and j
         self.matrix = []
-        # request format: (<request time>, <pickup point>, <dropoff point>, <number of passengers>)
         self.requests = []
-        # each vehicle is represented by its capacity
-        self.vehicles = []
-        # initial state is a tuple of tuples
-        # where each tuple represents a request state
-        # format: (<request state>) if request have not been picked up
-        #         (<request state>, <time>, <vehicle>) any other case
-        self.initial = ()
-        if fh:
-            self.load(fh)
+        self.cars = []
+        self.car_set = []
+        self.initial = State(None, None, None)
 
     def load(self, fh):
-        """Loads a problem from file object fh.
-
-        Args:
-            fh (file object): File object containing the problem instance.
-        
-        Usage:
-            The file object must be opened in read mode and the file must be
-            formatted as follows without a specific order of the sections:
-                P <number of points>
-                <matrix of distances>
-                R <number of requests>
-                <requests>
-                V <number of vehicles>
-                <vehicle capacities>
-                Where:
-                    <matrix of distances> is a upper triangular matrix of floats or integers
-                    <requests> are represented by a line with the following format:
-                        <request time> <pickup point> <dropoff point> <number of passengers>
-                    <vehicle capacities> are represented by a line with a single integer
-                
-        Side effects:
-            Initializes the attributes of the FleetProblem object.
-
-        """
-        self.__init__()
         p = 0
         r = 0
         v = 0
         for line in fh:
-            # Parse the input file line by line
             if p > 0:
-                # Read p lines with matrix distances
                 self.matrix[self.P - p - 1] = [0] * (self.P - p) + [float(x) for x in line.split()]
                 p -= 1
             elif r > 0:
-                # Read r lines of requests
                 self.requests.append([float(line.split()[0])] + [int(x) for x in line.split()[1:]])
                 r -= 1
             elif v > 0:
-                # Read v lines of vehicle capacities
-                self.vehicles.append(int(line))
+                self.cars.append(int(line))
                 v -= 1
             elif line.startswith('P'):
-                # If the line starts with 'P', read the number of points
                 self.P = int(line[2:])
                 self.matrix = [[0] * self.P] * self.P
                 p = self.P - 1
             elif line.startswith('R'):
-                # If the line starts with 'R', read the number of requests
                 self.R = int(line[2:])
                 r = self.R
             elif line.startswith('V'):
-                # If the line starts with 'V', read the number of vehicles
                 self.V = int(line[2:])
                 v = self.V
         
         self.matrix_triangulation()
-        self.initial = State((0,) for _ in range(len(self.requests)))
+        self.car_set = sorted(self.cars, reverse=True)
 
-        indexed_vehicles = list(enumerate(self.vehicles))
-        topR = sorted(indexed_vehicles, key=lambda x: x[1], reverse=True)[:self.R]
-        self.vehicles = {index: value for index, value in topR}
+        self.initial = State(tuple((0,) for _ in range(self.R)), {}, self.car_set)
 
     def matrix_triangulation(self):
         """Fills in the lower triangle of the distance 
@@ -333,100 +280,57 @@ class FleetProblem(search.Problem):
         
         return cost
     
-    def result(self, state, action):
-        """
-        Returns the state that results from executing action in state.
-        The action must be one of self.actions(state).
-        
-        Args:
-            state (tuple): A tuple representing a state.
-            action (tuple): A tuple representing an action.
-            
-        Usage:
-            The action format is as follows:
-                (<type of action>, <vehicle>, <request>, <action time>)
-            The state is a tuple where each position is formated as follows:
-                (<request state>) if request have not been picked up
-                (<request state>, <time>, <vehicle>) any other case
-                
-        Returns:
-            tuple: The resulting state.
-            
-        """
+    def result(self, state: State, action):
+        FleetProblem.counter += 1
         car = action[1]
         request = action[2]
         time = action[3]
-        requests = State(state[:request] + (((1, time, car,),) if state[request][0] == 0 else ((2, time, car,),)) + state[request + 1:])
-        return requests
-    
-    def actions(self, state):
-        """
-        Returns the actions that can be executed in the given state.
-        
-        Args:
-            state (tuple): A tuple representing a state.
-            
-        Usage:
-            The state is a tuple where each position is formated as follows:
-                (<request state>) if request have not been picked up
-                (<request state>, <time>, <vehicle>) any other case
-            The return list positions are actions that are formated as follows:
-                (<type of action>, <vehicle>, <request>, <action time>)
-        
-        Returns:
-            list: A list of actions that can be executed in the given state.
-        """
-        # Actions are formated as follows:
-        # (<type of action>, <vehicle>, <request>, <action time>)
-        actions = []
-        # Picks is a list of requests that have not been picked up
-        picks = []
-        # Drops is a list of tuples (request, vehicle) where request has been picked up and not dropped off
-        drops = []
-        # Pos is a list of positions of the vehicles
-        pos = [0] * self.V
-        # Time is a list of times of the vehicles
-        time = [0] * self.V
-        # Occupation is a list of the occupation of the vehicles
-        occupation = [0] * self.V
 
-        for i, r in enumerate(state):
+        req_state = state.req_state
+        car_set: list = state.car_set.copy()
+        cars = state.cars.copy()
+
+        current_occupation = 0
+        old_need = 0
+        new_need = 0
+        if car in cars:
+            _, _, current_occupation, old_need = cars[car]
+        new_occuption = current_occupation + (self.requests[request][3] if req_state[request][0] == 0 else  (-self.requests[request][3]))
+        if new_occuption > old_need:
+            if old_need > 0:
+                car_set.append(old_need)
+                car_set = sorted(car_set, reverse=True)
+            new_need_index = first_higher(car_set, new_occuption)
+            new_need = car_set.pop(new_need_index)
+        else:
+            new_need = old_need
+        
+        position = self.requests[request][1] if req_state[request][0] == 0 else self.requests[request][2]
+        cars[car] = (time, position, new_occuption, new_need)
+
+        req_state = req_state[:request] + (((1, time, car,),) if req_state[request][0] == 0 else ((2, time, car,),)) + req_state[request + 1:]
+        return State(req_state, cars, car_set)
+    
+    def actions(self, state: State):
+        actions = []
+
+        for i, r in enumerate(state.req_state):
             s = r[0]
             if s == 0:
-                # Add request to picks list
-                picks.append(i)
+                if state.car_set and self.requests[i][3] <= state.car_set[0]:
+                    t = self.matrix[0][self.requests[i][1]]
+                    actions.append(('Pickup', len(state.cars), i, max(t, self.requests[i][0])))
+                for v in state.cars:
+                    car = state.cars[v]
+                    # car = (time, position, current occupation, occupation needed so far)
+                    if car[2] + self.requests[i][3] <= car[3] or (state.car_set and car[2] + self.requests[i][3] <= state.car_set[0]):
+                        t = car[0] + self.matrix[car[1]][self.requests[i][1]]
+                        actions.append(('Pickup', v, i, max(t, self.requests[i][0])))
             elif s == 1:
-                # Add request to drops list
-                t = r[1]
-                car = r[2]
-                drops.append((i, car))
-                # Update occupation
-                occupation[car] += self.requests[i][3]
-                # Update time and position (the vehicle position and time are conditioned by last action executed)
-                if t > time[car]:
-                    time[car] = t
-                    pos[car] = self.requests[i][1]
-            else:
-                t = r[1]
-                car = r[2]
-                # Update time and position (the vehicle position and time are conditioned by last action executed)
-                if t > time[car]:
-                    time[car] = t
-                    pos[car] = self.requests[i][2]
-
-        for i in picks:
-            # Add pickup action for each vehicle that can pick up the request
-            for v in self.vehicles.keys():
-                # Vehicle must have enough capacity to pick up the request
-                if occupation[v] + self.requests[i][3] <= self.vehicles[v]:
-                    t = time[v] + self.matrix[pos[v]][self.requests[i][1]]
-                    # The pickup time must be equal or greater than the request time
-                    actions.append(('Pickup', v, i, t if t >= self.requests[i][0] else self.requests[i][0]))
-
-        # Add dropoff action to the actions list for each request that has been picked up and not dropped off
-        for i, v in drops:
-            t = time[v] + self.matrix[pos[v]][self.requests[i][2]]
-            actions.append(('Dropoff', v, i, t))
+                c = r[2]
+                car = state.cars[c]
+                t = car[0] + self.matrix[car[1]][self.requests[i][2]]
+                actions.append(('Dropoff', c, i, t))
         
         return actions
     
@@ -446,7 +350,7 @@ class FleetProblem(search.Problem):
         Returns:
             bool: True if state is a goal state, False otherwise.
         """
-        return all([r[0] == 2 for r in state])
+        return all(r[0] == 2 for r in state.req_state)
 
     def path_cost(self, c, state1, action, state2):
         """
@@ -475,7 +379,7 @@ class FleetProblem(search.Problem):
         if not self.is_dropoff(action):
             return c + self.get_action_time(action) - self.get_request_time(action)
         else:
-            return c + self.get_action_time(action) - state1[request][1] - self.get_trip_time(action)
+            return c + self.get_action_time(action) - state1.req_state[request][1] - self.get_trip_time(action)
 
     def get_best_cost(self, state, car, requests, position, time):
         best_cost = float('inf')
@@ -488,6 +392,7 @@ class FleetProblem(search.Problem):
                 t_copy += self.matrix[p_copy][self.requests[i][2]]
                 p_copy = self.requests[i][2]
                 action = ('Dropoff', car, i, t_copy)
+                FleetProblem.counter -= 1
                 aux = self.result(copy, action)
                 c_copy = self.path_cost(c_copy, copy, action, aux)
                 copy = aux
@@ -495,26 +400,23 @@ class FleetProblem(search.Problem):
                 best_cost = c_copy
         return best_cost
     
-    def get_ocupation(self, requests):
-        return sum([self.requests[i][3] for i in requests])
-    
-    def get_fastest_dropoff(self, cars, capacity, destination):
-        best_time = float('inf')
-        for c in cars.keys():
-            car = cars[c]
-            if self.vehicles[c] >= capacity:
-                for perm in itertools.permutations(car[2]):
-                    time = car[0]
-                    position = car[1]
-                    for r in perm:
-                        if self.vehicles[c] - self.get_ocupation(perm) >= capacity:
-                            break
-                        time += self.matrix[position][self.requests[r][2]]
-                        position = self.requests[r][2]
-                    time += self.matrix[position][destination]
-                    if time < best_time:
-                        best_time = time
-        return best_time
+    # def get_fastest_dropoff(self, cars, capacity, destination):
+    #     best_time = float('inf')
+    #     for c in cars:
+    #         car = cars[c]
+    #         if self.cars[c] >= capacity:
+    #             for perm in itertools.permutations(car[2]):
+    #                 time = car[0]
+    #                 position = car[1]
+    #                 for r in perm:
+    #                     if self.vehicles[c] - self.get_ocupation(perm) >= capacity:
+    #                         break
+    #                     time += self.matrix[position][self.requests[r][2]]
+    #                     position = self.requests[r][2]
+    #                 time += self.matrix[position][destination]
+    #                 if time < best_time:
+    #                     best_time = time
+    #     return best_time
     
     def h(self, node):
         cost_left = 0
@@ -525,30 +427,20 @@ class FleetProblem(search.Problem):
         
         # Calculate cars positions, times and requests pending
         # cars = {car_index: (time_of_the_car, position_of_the_car, (requests_to_dropoff,))}
-        for i, r in enumerate(state):
+        for i, r in enumerate(state.req_state):
             if r[0] == 1:
                 car = r[2]
                 if car not in cars:
-                    cars[car] = (r[1], self.requests[i][1], (i,))
-                elif r[1] > cars[r[2]][0]:
-                    cars[car] = (r[1], self.requests[i][1], cars[car][2] + (i,))
+                    cars[car] = (i,)
                 else:
-                    cars[car] = (cars[car][0], cars[car][1], cars[car][2] + (i,))
-            elif r[0] == 2:
-                car = r[2]
-                if car not in cars:
-                    cars[car] = (r[1], self.requests[i][2], ())
-                elif r[1] > cars[r[2]][0]:
-                    cars[car] = (r[1], self.requests[i][2], cars[car][2])
+                    cars[car] = cars[car] + (i,)
         
         # Calculate best order to dropoff requests for each car
 
         # get_best_cost does all the permutations of the requests
         #  to dropoff and returns the cost of the best permutation
-        for i in cars.keys():
-            car = cars[i]
-            if car[2]:
-                cost_left += self.get_best_cost(state, i, car[2], car[1], car[0])
+        for i in cars:
+            cost_left += self.get_best_cost(state, i, cars[i], state.cars[i][1], state.cars[i][0])
         
         # Search the first car to arrive at each request and calculate the cost so far
         
@@ -558,20 +450,18 @@ class FleetProblem(search.Problem):
         # If no request has capacity to pickup, the get_fastest_dropoff 
         # returns the arrival at pickup point of the car that can get 
         # fastest the capacity to pickup
-        for i, r in enumerate(state):
+        for i, r in enumerate(state.req_state):
             if r[0] == 0:
-                if any([c not in cars and self.vehicles[c] >= self.requests[i][3] for c in self.vehicles.keys()]):
+                if state.car_set:
                     best_arr_time = self.matrix[0][self.requests[i][1]]
                 else:
                     best_arr_time = float('inf')
-                for c in cars.keys():
-                    car = cars[c]
-                    if self.get_ocupation(car[2]) + self.requests[i][3] <= self.vehicles[c]:
-                        arr_time = car[0] + self.matrix[car[1]][self.requests[i][1]]
-                        if arr_time < best_arr_time:
-                            best_arr_time = arr_time
-                if best_arr_time == float('inf'):
-                    best_arr_time = self.get_fastest_dropoff(cars, self.requests[i][3], self.requests[i][1])
+                for c in state.cars:
+                    car = state.cars[c]
+                    arr_time = car[0] + self.matrix[car[1]][self.requests[i][1]]
+                    if arr_time < best_arr_time:
+                        best_arr_time = arr_time
+                    
                 cost_left += max(best_arr_time - self.requests[i][0], 0)
 
         return cost_left
@@ -585,3 +475,9 @@ class FleetProblem(search.Problem):
             list: A list of actions representing a solution to the problem.
         """
         return search.astar_search(self, h=self.h, display=True).solution()
+
+def first_higher(list: list, x: int) -> int:
+    for i in range(len(list)):
+        if list[i] < x:
+            return i - 1
+    return -1
